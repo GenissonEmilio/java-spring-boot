14517ba7666b6f6e35d215ef45604125208e29ca
Genisson Emilio <esgenisson@gmail.com>
2025-05-07 09:57:30 -0300

    Update User.java

--- backend/src/main/java/com/api/backend/models/User.java
+++ backend/src/main/java/com/api/backend/models/User.java
@@ -1,5 +1,6 @@
 package com.api.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -22,6 +23,7 @@ public class User {
     @Column(name = "username", length = 100, nullable = false, unique = true)
     private String username;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
     private String password;
 
