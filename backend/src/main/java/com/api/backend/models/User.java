407bf29884dc161152e8d913269264310f3f6c84
Genisson Emilio <esgenisson@gmail.com>
2025-05-07 10:05:20 -0300

    Update User.java

--- backend/src/main/java/com/api/backend/models/User.java
+++ backend/src/main/java/com/api/backend/models/User.java
@@ -2,16 +2,17 @@ package com.api.backend.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
-    public interface createUser {}
-    public interface updateUser {}
+    public interface CreateUser {}
+    public interface UpdateUser {}
 
     public static final String TABLE_NAME = "user";
 
@@ -21,10 +22,16 @@ public class User {
     private Long id;
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 3, max = 100)
     private String username;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
     //private List<Task> tasks = new ArrayList<Task>();
