08e21ed282079c8eb7a95039f18d3975a15e5899
Genisson Emilio <esgenisson@gmail.com>
2025-05-09 09:54:18 -0300

    create task repository

---
+++ backend/src/main/java/com/api/backend/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.api.backend.repositories;
+
+import com.api.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
