553bd7ff5e45b0866a49a1b220fdd6fe43915f99
Genisson Emilio <esgenisson@gmail.com>
2025-05-12 10:20:46 -0300

    Create User Controller and SecurityConfig

---
+++ backend/src/main/java/com/api/backend/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.api.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Libera tudo para teste
+                )
+                .httpBasic(Customizer.withDefaults()); // Habilita autenticação básica se necessário
+
+        return http.build();
+    }
+}
--- backend/src/main/java/com/api/backend/models/User.java
+++ backend/src/main/java/com/api/backend/models/User.java
@@ -1,5 +1,6 @@
 package com.api.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
@@ -73,6 +74,7 @@ public class User {
         this.username = username;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return tasks;
     }
